a5fe7d6a747268a1641f32c8f8b073fb022af93e
Jeff MAURY <jmaury@redhat.com>
2024-02-28 14:16:48 +0100

    fix: switch default port to 8000 (#6)
    Fixes #5
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- chat/run.sh
+++ chat/run.sh
@@ -15,4 +15,4 @@
 # limitations under the License.
 #
 # SPDX-License-Identifier: Apache-2.0
-python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8001} --n_gpu_layers 0
+python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8000} --n_gpu_layers 0
