c55c6df39b88673a67c1cfef40881f3826d489ae
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-04-11 14:09:21 +0200

    Update chat/run.sh
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- chat/run.sh
+++ chat/run.sh
@@ -16,7 +16,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 if [ ${MODEL_PATH} ]; then
-    python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8001} --n_gpu_layers ${GPU_LAYERS:=0} --clip_model_path ${CLIP_MODEL_PATH:=None} --chat_format ${CHAT_FORMAT:="llama-2"}
+    python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8001} --n_gpu_layers ${GPU_LAYERS:=0} --clip_model_path ${CLIP_MODEL_PATH:=None} --chat_format ${MODEL_CHAT_FORMAT:="llama-2"}
     exit 0
 fi
 
