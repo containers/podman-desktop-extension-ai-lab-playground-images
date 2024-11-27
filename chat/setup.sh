cb90220bf617c24b848a210ded3f773714a828bd
Jeff MAURY <jmaury@redhat.com>
2024-11-27 14:01:32 +0100

    fix: use rebase instead of apply
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- chat/setup.sh
+++ chat/setup.sh
@@ -2,5 +2,4 @@ git clone https://github.com/abetlen/llama-cpp-python --no-checkout --recurse-su
 cd llama-cpp-python
 git checkout v0.3.2
 git submodule update
-curl -O -L https://github.com/abetlen/llama-cpp-python/pull/1552.diff
-git apply 1552.diff
+git rebase ffc47e5a95cf31fe6d9d6d7953d5eb17fdcc8513
