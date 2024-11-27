6a9f3bc2305488994bbf2e40170f165bceeaf1b6
Jeff MAURY <jmaury@redhat.com>
2024-11-27 14:27:07 +0100

    fix: fix setup file
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- chat/setup.sh
+++ chat/setup.sh
@@ -2,4 +2,5 @@ git clone https://github.com/abetlen/llama-cpp-python --no-checkout --recurse-su
 cd llama-cpp-python
 git checkout v0.3.2
 git submodule update
+git fetch origin pull/1552/head
 git rebase ffc47e5a95cf31fe6d9d6d7953d5eb17fdcc8513
