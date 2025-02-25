65db0b873c90ef3909ecf7d8d70af391d1c0526f
Jeff MAURY <jmaury@redhat.com>
2025-02-25 18:35:31 +0100

    Revert "fix: update image to support usage info (#81)"
    This reverts commit ad0b3ec9e53c2e9807be1341ae3481921947328c.
    
    Fixes #98
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- chat/setup.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/abetlen/llama-cpp-python --no-checkout --recurse-submodules
-cd llama-cpp-python
-git checkout v0.3.2
-git submodule update
-git fetch origin pull/1552/head
-git rebase ffc47e5a95cf31fe6d9d6d7953d5eb17fdcc8513
